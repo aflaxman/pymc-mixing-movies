e004b9fc7e53528e080be28d799fcf8921a64b41
abie <abie@riseup.net>
2011-01-25 13:06:23 -0800

    add pymc model for diagonal

--- models.py
+++ models.py
@@ -7,3 +7,14 @@ import pylab as pl
 import pymc as mc
 
 # simple models for some uniformly distributed subsets of the plane
+def diagonal():
+    X = mc.Uniform('X', lower=-1., upper=1., value=[0., 0.])
+
+    @mc.potential
+    def near_diag(X=X):
+        if abs(X[0] - X[1]) < .05:
+            return 0
+        else:
+            return -pl.inf
+
+    return mc.Model(vars())
--- tests.py
+++ tests.py
@@ -7,8 +7,10 @@ matplotlib.use("AGG")
 import models
 
 class TestClass:
-   def setUp(self):
-      pass
+    def setUp(self):
+        pass
 
-   def test_models(self):
-       assert False, 'Write test, fail, write code, pass'
+    def test_models(self):
+        m1 = models.diagonal()
+        assert hasattr(m1, 'X'), 'model should have stoch X as attribute'
+
