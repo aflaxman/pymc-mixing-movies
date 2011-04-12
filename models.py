28f8b1c44525c40f57c7de852461ca56a20cb58c
abie <abie@riseup.net>
2011-04-12 11:01:45 -0700

    add banana distrubtion
    requested by mariel, defined in Haario, Saksman, Tamminen, Adaptive proposal distribution for random walk Metropolis algorithm, Computational Statistics 1999

--- models.py
+++ models.py
@@ -55,6 +55,32 @@ def x_diagonal(step='Metropolis', iters=5000):
                           [ 1,-1], [.9, -1], [  0,-.1], [-.9,-1]])
     return mod
 
+def banana(dim=2, b=.03, step='Metropolis', iters=5000):
+    """ The non-linear banana-shaped distributions are constructed
+    from the Gaussian ones by 'twisting' them as follows.  Let f be
+    the density of the multivariate normal distribution N(0, C_1) with
+    the covariance again given by C_1 = diag(100, 1, ..., 1).  The
+    density function of the 'twisted' Gaussian with the nonlinearity
+    parameter b > 0 is given by f_b = f \circ \phi_b, where the
+    function \phi)b = (x_1, x_2 + b x_1^2 - 100b, x_3, ..., x_n).
+    """
+    assert dim >= 2, 'banana must be dimension >= 2'
+    C_1 = pl.ones(dim)
+    C_1[0] = 100.
+    X = mc.Uninformative('X', value=pl.zeros(dim))
+
+    @mc.potential
+    def banana(X=X, tau=C_1**-1, b=b):
+        phi_X = pl.copy(X)
+        phi_X *= 30. # rescale X to match scale of other models
+        phi_X[1] = phi_X[1] + b*phi_X[0]**2 - 100*b
+
+        return mc.normal_like(phi_X, 0., tau)
+
+    mod = setup_and_sample(vars(), step, iters)
+    #mod.plot_distribution = show_banana
+    return mod
+
 
 def setup_and_sample(vars, step, iters=5000):
     mod = mc.MCMC(vars)
@@ -69,7 +95,7 @@ def setup_and_sample(vars, step, iters=5000):
         reload(steppers)
         mod.use_step_method(steppers.HRAM, mod.X, proposal_sd=.01)
     elif step == 'Metropolis':
-        mod.use_step_method(mc.Metropolis, mod.X)
+        mod.use_step_method(mc.Metropolis, mod.X, proposal_sd=.01)
     else:
         raise Exception, 'Unrecognized Step Method'
     mod.sample(iters)
